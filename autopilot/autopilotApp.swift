f65c94ad8755d6b5b2e481d6376cefe963d8a140
Faisal Sayed <faisal.sayed502@gmail.com>
2024-12-23 20:20:57 -0500

    Initial Commit

---
+++ autopilot/autopilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  autopilotApp.swift
+//  autopilot
+//
+//  Created by Faisal Sayed on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct autopilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
